0b0442e16102f715a80ca2608d11a33ff1103c77
murugan <murugan@murugans-mac-mini.local>
2017-06-17 21:45:48 +0530

    Initial Commit

---
+++ Intro View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Intro View
+//
+//  Created by murugan on 17/06/17.
+//  Copyright © 2017 murugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Intro View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Intro View
+//
+//  Created by murugan on 17/06/17.
+//  Copyright © 2017 murugan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
